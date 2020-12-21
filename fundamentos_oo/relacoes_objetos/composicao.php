8f4619ec6befd45cfcebc8cfd4a81588bf37d289
Cassius Souza <cassius.sanches@gmail.com>
2020-12-21 20:22:15 -0300

    Exemplo de composição

--- fundamentos_oo/relacoes_objetos/composicao.php
+++ fundamentos_oo/relacoes_objetos/composicao.php
@@ -2,6 +2,10 @@
 require_once 'classes/Produto.php';
 require_once 'classes/Caracteristica.php';
 
+// Composição é um tipo de relacionamento entre objetos, é uma associação especial
+// Não possui apenas um apontamento para o outro, ele contém vários outros objetos
+// Chamado todo - parte
+// Um objeto maior que contém partes menores, objetos filhos
 $produto = new Produto('Chocolate', 100, 5);
 
 $produto->addCaracteristica('Cor', 'Marrom');
