e0059efcf557d775846be7a228e5f37b4e06e5d5
Cassius Souza <cassius.sanches@gmail.com>
2020-12-22 08:49:06 -0300

    Exemplos de uso de classe abstrata

---
+++ fundamentos_oo/relacoes_objetos/classe_abstrata.php
@@ -0,0 +1,6 @@
+<?php
+require_once './classes/Conta.php';
+// Uma classe abstrata é uma classe incompleta, serva para estruturar outras classes
+// Seu propósito é construir outras classes derivadas a partir dela
+
+$conta = new Conta;
