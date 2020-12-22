ec8910b424fcf3816068adec288098e4b6c75cad
Cassius Souza <cassius.sanches@gmail.com>
2020-12-22 10:36:40 -0300

    Atributos objetos

---
+++ fundamentos_oo/relacoes_objetos/objetos_atributos.php
@@ -0,0 +1,17 @@
+<?php
+
+class Funcionario
+{
+  public $nome;
+  public $salario;
+  public $departamento;
+}
+
+$jose = new Funcionario;
+$jose->nome = 'José da Silva';
+$jose->salario = 3000;
+$jose->departamento = 'Financeiro';
+
+echo "<pre>";
+print_r(get_object_vars($jose));
+echo "</pre>";
