6dc49b5753414f5bbabfca88febfb3f1052517e9
Cassius Souza <cassius.sanches@gmail.com>
2020-12-22 09:25:21 -0300

    Encapsulamento - atributos publicos, sem proteção

---
+++ fundamentos_oo/relacoes_objetos/public.php
@@ -0,0 +1,23 @@
+<?php
+
+// Encapsulamento
+// Capacidade de uma classe de proteger seus atributos
+// Proteger a informação que ela carrega
+// Proteger de ser enviado um dado inconsistente
+// Seus atributos não devem ser acessados pelo "mundo externo", deve ser feito por meio de métodos
+
+class Pessoa
+{
+  public $nome;
+  public $endereco;
+  public $nascimento;
+}
+
+$pessoa = new Pessoa;
+$pessoa->nome = 'Maria Josefa';
+$pessoa->endereco = 'Rua XPTO';
+$pessoa->nascimento = '01 de janeiro de 2021';
+
+echo "<pre>";
+print_r($pessoa);
+echo "</pre>";
