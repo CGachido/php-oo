0962e18e9a463e0aeea220b1f67c257491328696
Cassius Souza <cassius.sanches@gmail.com>
2020-12-22 10:19:53 -0300

    Membros da classe, acessando de qualquer lugar

---
+++ fundamentos_oo/relacoes_objetos/constante_classe.php
@@ -0,0 +1,43 @@
+<?php
+class Pessoa
+{
+  // Membros de classe
+  // Valor global de um objeto de classe
+  // Constante ou Atributo de classe
+  private $nome;
+  private $genero;
+  const GENEROS = [
+    'M' => 'Masculino',
+    'F' => 'Feminino',
+  ];
+
+  public function __construct($nome, $genero)
+  {
+    $this->nome = $nome;
+    $this->genero = $genero;
+  }
+
+  public function getNome()
+  {
+    return $this->nome;
+  }
+
+  function getNomeGenero()
+  {
+    return self::GENEROS[$this->genero];
+  }
+}
+
+var_dump(Pessoa::GENEROS['M']);
+$p1 = new Pessoa('Maria da Silva', 'F');
+$p2 = new Pessoa('José da Silva', 'M');
+
+echo "<pre>";
+print $p1->getNome();
+echo "<br>";
+print $p1->getNomeGenero();
+echo "<hr>";
+print $p2->getNome();
+echo "<br>";
+print $p2->getNomeGenero();
+echo "</pre>";
