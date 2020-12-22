1282350e93594d5b106162fe2d50f5fd589d9b75
Cassius Souza <cassius.sanches@gmail.com>
2020-12-22 12:50:48 -0300

    Adicionando interface ao Produto

---
+++ fundamentos_oo/relacoes_objetos/classes/Produto2.php
@@ -0,0 +1,52 @@
+<?php
+
+class Produto implements OrcavelInterface
+{
+  // Associação é quando um objeto possui um ponteiro para outro objeto
+  // Um objeto possui uma referencia para outro objeto, objeto A conhece objeto B
+  // Produto 0..* -> 1 Fabricante
+
+  private $descricao;
+  private $estoque;
+  private $preco;
+  private $fabricante;
+  private $caracteristicas;
+
+  public function __construct($descricao, $estoque, $preco)
+  {
+    $this->descricao = $descricao;
+    $this->estoque = $estoque;
+    $this->preco = $preco;
+    $this->caracteristicas = [];
+  }
+
+  public function getDescricao()
+  {
+    return $this->descricao;
+  }
+
+  public function getPreco()
+  {
+    return $this->preco;
+  }
+
+  public function setFabricate(Fabricante $fabricante)
+  {
+    $this->fabricante = $fabricante;
+  }
+
+  public function getFabricante()
+  {
+    return $this->fabricante;
+  }
+
+  public function addCaracteristica($nome, $valor)
+  {
+    $this->caracteristicas[] = new Caracteristica($nome, $valor);
+  }
+
+  public function getCaracteristicas()
+  {
+    return $this->caracteristicas;
+  }
+}
