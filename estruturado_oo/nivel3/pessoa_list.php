3eb9cafeb1ee0b504c4aff9d1794321a31d39086
Cassius Souza <cassius.sanches@gmail.com>
2020-12-24 14:36:25 -0300

    Separação PHP e HTML listagem de pessoas

--- estruturado_oo/nivel3/pessoa_list.php
+++ estruturado_oo/nivel3/pessoa_list.php
@@ -1,75 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Listagem de Pessoas</title>
-  <link rel="stylesheet" href="css/list.css">
-</head>
+<?php
+$conn = pg_connect('host=localhost port=5432 dbname=curso_oo user=postgres password=pg@password');
+if (!empty($_GET['action']) && $_GET['action'] === 'delete') {
+  $id = (int) $_GET['id'];
+  $sql = "DELETE FROM pessoas WHERE id = {$id}";
+  $result = pg_query($conn, $sql);
+}
 
-<body>
+$result = pg_query($conn, "SELECT * FROM pessoas ORDER BY id");
+$itens = "";
+while ($row = pg_fetch_assoc($result)) {
+  $item = file_get_contents('html/item.html');
+  $item = str_replace('{id}', $row['id'], $item);
+  $item = str_replace('{nome}', $row['nome'], $item);
+  $item = str_replace('{endereco}', $row['endereco'], $item);
+  $item = str_replace('{bairro}', $row['bairro'], $item);
+  $item = str_replace('{telefone}', $row['telefone'], $item);
+  $item = str_replace('{email}', $row['email'], $item);
+  $itens .= $item;
+}
 
-  <table border="1">
-    <thead>
-      <tr>
-        <th></th>
-        <th></th>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Endereço</th>
-        <th>Bairro</th>
-        <th>Telefone</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      $conn = pg_connect('host=localhost port=5432 dbname=curso_oo user=postgres password=pg@password');
-      if (!empty($_GET['action']) && $_GET['action'] === 'delete') {
-        $id = (int) $_GET['id'];
-        $sql = "DELETE FROM pessoas WHERE id = {$id}";
-        $result = pg_query($conn, $sql);
-      }
+$list = file_get_contents('html/list.html');
+$list = str_replace('{itens}', $itens, $list);
 
-      $result = pg_query($conn, "SELECT * FROM pessoas ORDER BY id");
-
-      while ($row = pg_fetch_assoc($result)) {
-        $id       = $row['id'];
-        $nome     = $row['nome'];
-        $endereco = $row['endereco'];
-        $bairro   = $row['bairro'];
-        $telefone = $row['telefone'];
-        $email    = $row['email'];
-        $idCidade = $row['id_cidade'];
-
-        print '<tr>';
-        print "<td>
-                <a href='pessoa_form.php?action=edit&id={$id}'>
-                  <img src='img/edit.svg' style='width: 17px;'>
-                </a>
-              </td>";
-        print "<td>
-              <a href='pessoa_list.php?action=delete&id={$id}'>
-                <img src='img/remove.svg' style='width: 17px;'>
-              </a>
-            </td>";
-        print "<td>{$id}</td>";
-        print "<td>{$nome}</td>";
-        print "<td>{$endereco}</td>";
-        print "<td>{$bairro}</td>";
-        print "<td>{$telefone}</td>";
-        print "<td>{$email}</td>";
-        print '</tr>';
-      }
-      ?>
-    </tbody>
-  </table>
-
-  <button onclick="window.location='pessoa_form.php'">
-    <img src="img/insert.svg" style="width: 17px;">
-  </button>
-
-</body>
-
-</html>
+print $list;
+?>
