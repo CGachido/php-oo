54b39032e0ba4367cf0baf04e1547461e06a0bc4
Cassius Souza <cassius.sanches@gmail.com>
2020-12-26 17:02:44 -0300

    Usando gerenciador de pacotes COMPOSER

---
+++ topicos_especiais_oo/composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b4530a9aaf015a98dc0a14c25dd5c741f81d5128',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b4530a9aaf015a98dc0a14c25dd5c741f81d5128',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.2.0',
+      'version' => '6.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e38888a75c070304ca5514197d4847a59a5c853f',
+    ),
+  ),
+);
