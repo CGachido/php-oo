5981627526ad11ad5e4a9e2c04d68272820b1a2e
Cassius Souza <cassius.sanches@gmail.com>
2020-12-27 09:36:50 -0300

    Adicionando caracter de quebra de linha

--- 5-persistencia/classes/api/LoggerTXT.php
+++ 5-persistencia/classes/api/LoggerTXT.php
@@ -4,7 +4,7 @@ class LoggerTXT extends Logger
 {
   public function write($message)
   {
-    $text = date("Y-m-d H:i:s") . ": {$message}";
+    $text = date("Y-m-d H:i:s") . ": {$message} <br>\n";
     $handler = fopen($this->filename, 'a');
     fwrite($handler, $text);
     fclose($handler);
