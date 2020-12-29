40df910e223fa4110e8ce678eebdf046d60c7bd4
Cassius Souza <cassius.sanches@gmail.com>
2020-12-29 18:28:05 -0300

    Criação de componente DataGrid (table)

---
+++ 7-formularios_e_listagens/Lib/Livro/Widgets/Datagrid/Datagrid.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Livro\Widgets\Datagrid;
+
+use Livro\Control\ActionInterface;
+
+class Datagrid
+{
+
+	private $columns;
+	private $actions;
+	private $items;
+
+	public function __construct()
+	{
+		$this->columns 	= [];
+		$this->actions 	= [];
+		$this->items 	= [];
+	}
+
+	public function addColumn(DatagridColumn $object)
+	{
+		$this->columns[] = $object;
+	}
+
+	public function addAction($label, ActionInterface $action, $field, $image = null)
+	{
+		$this->actions[] = [
+			'label' 	=> $label,
+			'action' 	=> $action,
+			'field' 	=> $field,
+			'image' 	=> $image,
+		];
+	}
+
+	public function addItem($object)
+	{
+		$this->items[] = $object;
+	}
+
+	public function getColumns()
+	{
+		return $this->columns;
+	}
+
+	public function getActions()
+	{
+		return $this->actions;
+	}
+
+	public function getItems()
+	{
+		return $this->items;
+	}
+
+	public function clear()
+	{
+		$this->items = [];
+	}
+}
