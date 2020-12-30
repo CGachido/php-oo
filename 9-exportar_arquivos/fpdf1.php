7cfc5de84432b7e0cac1ab6d7d6b2563320afd44
Cassius Souza <cassius.sanches@gmail.com>
2020-12-30 16:54:48 -0300

    Bibliotecas para exportar PDF, RTF e XLS; classes para facilitar essas exportações

--- 9-exportar_arquivos/fpdf1.php
+++ 9-exportar_arquivos/fpdf1.php
@@ -7,48 +7,48 @@ $pdf = new FPDF('P', 'pt', 'A4');
 // define os atributos do PDF
 $pdf->SetTitle('Teste PDF 1');
 $pdf->SetAuthor('Pablo Dall\'Oglio');
-$pdf->SetCreator('PHP'.phpversion());
+$pdf->SetCreator('PHP' . phpversion());
 $pdf->SetKeywords('php, pdf');
 $pdf->SetSubject('Como criar um PDF com PHP 1');
 
-// adiciona uma página
+// adiciona uma pï¿½gina
 $pdf->AddPage();
 
 // define a fonte
-$pdf->SetFont('Arial','',12);
+$pdf->SetFont('Arial', '', 12);
 
-// marca os quatro cantos da página
+// marca os quatro cantos da pï¿½gina
 $pdf->Text(0, 12, 'X1');
 $pdf->Text(576, 12, 'X2');
 $pdf->Text(0, 840, 'X3');
 $pdf->Text(576, 840, 'X4');
 
-// vinte pontos de espaçamento
+// vinte pontos de espaï¿½amento
 $pdf->Ln(20);
 
-// imprie o título em azul centralizado
+// imprie o tï¿½tulo em azul centralizado
 $pdf->SetFont('Courier', 'B', 16);
 $pdf->SetTextColor(50, 50, 100);
 $pdf->SetY(70);
 $pdf->SetX(260);
-$pdf->Write(30, 'Título'); // não desloca o cursor
+$pdf->Write(30, 'Tï¿½tulo'); // nï¿½o desloca o cursor
 
-// vinte pontos de espaçamento
+// vinte pontos de espaï¿½amento
 $pdf->Ln(30);
 
-// cria variáveis de texto com repetições
+// cria variï¿½veis de texto com repetiï¿½ï¿½es
 $txt = str_repeat('write ', 30);
 
 $pdf->SetTextColor(100, 50, 50);  // tom de vermelho
 $pdf->SetFont('Times', 'B', 14);
-// imprime texto usando a função Write
-$pdf->Write(20, $txt); // não desloca o cursor
+// imprime texto usando a funï¿½ï¿½o Write
+$pdf->Write(20, $txt); // nï¿½o desloca o cursor
 
-// imprime texto após alterar coordenada X
+// imprime texto apï¿½s alterar coordenada X
 $pdf->Ln(40);
-$pdf->SetX(200); // altera a posição X
+$pdf->SetX(200); // altera a posiï¿½ï¿½o X
 $pdf->SetTextColor(50, 100, 50); // tom de verde
-$pdf->Write(20, 'SetX '. $txt);
+$pdf->Write(20, 'SetX ' . $txt);
 
 // imprime dados com hyperlink
 $pdf->Ln(40);
